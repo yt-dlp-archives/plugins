45540ec933915dc96b7e0e35896ad8cc6922c87a
Person <localboxcrox@gmail.com>
2024-02-24 08:40:42 -0500

    stuff

--- create_new_postprocessor.sh
+++ create_new_postprocessor.sh
@@ -7,6 +7,11 @@ if [ -z "$NAME" ]; then
     exit 1;
 fi
 
+if [ -e "./pluggables/$NAME/yt_dlp_plugins/$TYPE/$NAME.py" ]; then
+	printf "%s for %s already exists at %s\n" "$TYPE" "$NAME" "./pluggables/$NAME/yt_dlp_plugins/";
+    exit 1;
+fi
+
 mkdir -p "./pluggables/$NAME/yt_dlp_plugins/$TYPE";
 touch "./pluggables/$NAME/yt_dlp_plugins/$TYPE/$NAME.py";
 printf "Put your %s code in './pluggables/%s/yt_dlp_plugins/%s/%s.py'\n"  "$TYPE" "$NAME" "$TYPE" "$NAME";
