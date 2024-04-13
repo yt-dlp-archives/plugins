53968840873b1f463554ad66c403a8ed97e99478
Person <localboxcrox@gmail.com>
2024-04-13 19:30:31 -0400

    better plugin update mechanism

--- utils/update_plugin_list.py
+++ utils/update_plugin_list.py
@@ -12,13 +12,19 @@ project_dir = Path(__file__).parents[1]
 plgin_path = os.path.join(project_dir, "pluggables")
 
 plgin_list = []
+plgin_list_simple = "# PLUGINS LIST\n"
 for pname in os.listdir(plgin_path):
     ptypes = os.path.join(os.path.join(plgin_path, pname), "yt_dlp_plugins")
     for ptype in os.listdir(ptypes):
         if os.path.isfile(os.path.normpath(os.path.join(os.path.join(ptypes, ptype), f"{pname}.py"))):
             print(f"adding pluggin [{pname}] of type [{ptype}]")
             plgin_list.append({"plugin": pname, "type": ptype})
+            plgin_list_simple += f"- **{pname}** (*{ptype}*)\n"
 
 with open(os.path.join(project_dir, "pluggin_list.json"), "w") as json_file:
     json.dump({"list": plgin_list}, json_file, indent=4)
     json_file.close()
+
+with open(os.path.join(project_dir, "pluggin_list.md"), "w") as md_file:
+    md_file.write(plgin_list_simple)
+    md_file.close()
