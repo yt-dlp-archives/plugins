83c80e7fde2d0da78327057239433e0c73bf371f
Person <localboxcrox@gmail.com>
2024-04-13 14:11:58 -0400

    init3 for py util scripts

--- utils/install_plugin.py
+++ utils/install_plugin.py
@@ -7,11 +7,20 @@ if sys.version_info <= (3, 4):
 import urllib.request
 import json
 
-print("YTDLP ARCHIEVES PLUGINS SEARCH+INSTALLATION TOOL")
+print("YTDLP ARCHIEVES PLUGINS SEARCH+INSTALLATION TOOL\n")
 plg_nm = input("plugin name: ")
 plugin_list_json = json.loads(urllib.request.urlopen("https://raw.githubusercontent.com/yt-dlp-archives/plugins/main/pluggin_list.json").read())
 
+print("\n")
+worked_flag = 0
 for plgin in plugin_list_json["list"]:
     if plgin["plugin"] == plg_nm:
         # install
-        print()
+        print(f"install candidate found of type [{plgin["type"]}]")
+        worked_flag = 1
+
+if worked_flag == 0:
+    print(f"no installation candidate for plugin of name: \"{plg_nm}\"")
+    exit(1)
+else:
+    print("done")
